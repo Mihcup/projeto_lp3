eedc13268844c2beee2bd3166b798a9056dab0cf
Mihcup <millenacupolillo@gmail.com>
2024-06-18 08:41:30 -0300

    projeto_lp3

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,5 +1,5 @@
 #vamos usar do Flask 
-from flask import Flask
+from flask import Flask, render_template
 #importando a classe Flask do módulo flask 
 from validate_docbr import CPF, CNPJ
 
@@ -13,25 +13,29 @@ cnpj = CNPJ()
 #"Minha aplicação" -> nome (atributo) do objeto 
 #instancia um objeto flask que representa a aplicação
 
-#rota + função 
-#rota: definição de um padrão url 
-#função: função python com retorno (string, template, outro)
-
 #página home - / (padrão da home é o barra)
 @app.route("/")
 #toda vez que marcar / acessa a página home 
 def home(): 
-    return "<h1>Home page</h1>"
+    return render_template("home.html")
+
 #página contato - /contato 
 @app.route("/contato")
 def contato():
-    return "<h1>Contato</h1>"
+    return render_template("contatos.html")
+
 #página produtos - /produtos 
 @app.route("/produtos")
 def produtos():
-    return "<h1>Lista de produtos</h1>"
-#o que aparece na url -> é a rota 
+    lista_produtos=[
+        {"nome": "Coca-cola", "descricao":"Mata a sede"}, 
+        {"nome": "doritos", "descricao":"suja a mão"},
+        {"nome":"Chocolate", "descricao":"bom"}
+        #a lista poderia vir do bdd 
+    ]
+    return render_template("produtos.html",produtos=lista_produtos)
 
+#o que aparece na url -> é a rota 
 # /gerar_cpf (deve devolver um cpf aleatório)
 # /servicos (deve devolver um título "Nossos servicos")
 # /gerar_cnpj (deve devolver um cnpj aleatorio)
@@ -50,3 +54,24 @@ def servicos():
 def gerar_cnpj():
     cnpj_usuario = cnpj.generate(True)
     return "<div> O seu cnpj é "+ cnpj_usuario + "<div>"
+
+app.run()
+
+#códigos das últimas aulas: 
+
+#---------------------------------------------
+# #página contato - /contato 
+# @app.route("/contato")
+# def contato():
+#     return "<h1>Contato</h1>"
+
+#-----------------------------------------------
+# #página produtos - /produtos 
+# @app.route("/produtos")
+# def produtos():
+#     return "<h1>Lista de produtos</h1>"
+
+#---------------------------------------------
+#rota + função 
+#rota: definição de um padrão url 
+#função: função python com retorno (string, template, outro)
