f077a5419b3fdba36d866bb59b1d38567646fbed
Mihcup <millenacupolillo@gmail.com>
2024-06-20 15:03:37 -0300

    teste

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -40,22 +40,47 @@ def produtos():
 # /servicos (deve devolver um título "Nossos servicos")
 # /gerar_cnpj (deve devolver um cnpj aleatorio)
 
+#exercício 1 e 2
 @app.route("/gerar_cpf")
 def gerar_cpf():
     cpf_usuario = cpf.generate(True)
-    return f"<h1>Gerar CPF</h1><p>CPF: {cpf_usuario}"
+    return render_template("cpf.html",cpf=cpf_usuario)
 
-@app.route("/servicos")
-def servicos():
-    return "<h1>Nossos servicos</h1>"
-#o que aparece na url -> é a rota 
 
 @app.route("/gerar_cnpj")
 def gerar_cnpj():
     cnpj_usuario = cnpj.generate(True)
-    return "<div> O seu cnpj é "+ cnpj_usuario + "<div>"
+    return render_template("cnpj.html",cnpj=cnpj_usuario)
+
+
+#exercício 3
+@app.route("/como_utilizar")
+def como_utilizar(): 
+    return render_template("como_utilizar.html")
+
+@app.route("/politica_privacidade")
+def politica_privacidade(): 
+    return render_template("politica_privacidade.html")
+
+@app.route("/termos_de_uso")
+def termos_uso(): 
+    return render_template("termos_uso.html")
+
+@app.route("/casa")
+def home_2(): 
+    return render_template("home_2.html")
+
+@app.route("/produtos_2")
+def produto_2(): 
+    return render_template("produto_2.html")
+
+@app.route("/contatos_2")
+def contato_2(): 
+    return render_template("contato_2.html")
 
 app.run()
+
+
 
 #códigos das últimas aulas: 
 
